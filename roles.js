9e8ee1597e49bcd94705a72a3fada2033fa7a163
pelad0 <latintainc@gmail.com>
2016-05-31 16:39:38 -0300

    insertarRol 1.0

--- roles.js
+++ roles.js
@@ -1,27 +1,67 @@
-function insertarRol(db){
-	return function(req, res){
-		
-			}	
+function insertarRol(db) {
+	return function (req, res) {
+		console.log("entro el insertarRol");
+		var rol = db.get('rol');
+		var nuevoRol = {
+			"nombre": req.body.nombre,
+			"permisos": req.body.permisos
+		};
+		rol.find({ nombre: nuevoRol.nombre }, function (err, doc) {
+			if (err) throw err;
+			else {
+				if (doc.length != 0) {
+					res.send('Ya existe un rol con ese nombre.');
+				}
+				else {
+					rol.insert(nuevoRol, function (err, doc) {
+						if (err) throw err;
+						else {
+							res("El rol " + nuevoRol.nombre + " ha sido insertado con éxito");
+						}
+					});
+				}
+			}
+		});
+	}
 }
-function modificarRol(db){
-	return function(req, res){
-		
-			}	
+
+/* users.find({ user : user.user}, function( err, doc){
+				if(err) throw err;
+				else{
+					console.log(doc);
+					if(doc.length != 0){
+						res.send('Nombre de usuario no disponible.');
+					}
+					else{
+						users.insert(user, function( err, doc){
+							if(err) throw err;
+							else{
+								res.send('Binenvenido ' + user.user + '!!!!');
+							}
+						});
+						
+					}
+				}
+			})*/
+function modificarRol(db) {
+	return function (req, res) {
+
+	}
 }
-function borrarRol(db){
-	return function(req, res){
-		
-			}	
+function borrarRol(db) {
+	return function (req, res) {
+
+	}
 }
-function listarRol(db){
-	return function(req, res){
-		
-			}	
+function listarRol(db) {
+	return function (req, res) {
+
+	}
 }
-function asignarRol(db){
-	return function(req, res){
-		
-			}	
+function asignarRol(db) {
+	return function (req, res) {
+
+	}
 }
 module.exports.insertarRol = insertarRol;
 module.exports.modificarRol = modificarRol;
