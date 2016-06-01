5481e7aa060645f8b1e6a7818ffc1ea9e6697269
pelad0 <latintainc@gmail.com>
2016-06-01 19:27:58 -0300

    modificarRol ok

--- roles.js
+++ roles.js
@@ -28,14 +28,21 @@ function modificarRol(db) {
 	return function (req, res) {
 		var rol = db.get('rol');
 		var nuevoRol = {
+			"_id": req.body._id,
 			"nombre": req.body.nombre,
 			"permisos": req.body.permisos
 		};
-		rol.find({ nombre: req.body.nombre }, function (err, doc) {
+		rol.find({ _id: nuevoRol._id }, function (err, doc) {
 			if (err) throw err;
 			else {
 				if (doc.length != 0) {
-					rol.update({"nombre":nuevoRol.nombre},{"permisos": nuevoRol.permisos});
+					rol.update({"_id":nuevoRol._id},{"nombre": nuevoRol.nombre, "permisos": nuevoRol.permisos}, function(err, doc){
+						if(err) throw err;
+						else{
+							res.send("El rol se modifico con éxito");
+						}
+					});
+					
 				}
 				else {
 					res.send("No se registra ningun rol con ese nombre.");
