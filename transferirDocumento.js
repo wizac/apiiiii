561e1871b5334bc2048c411faa68e66456cf074f
ggpq22 <ggpq13@hotmail.com>
2016-06-02 17:18:44 -0300

    gonza

--- transferirDocumento.js
+++ transferirDocumento.js
@@ -3,25 +3,18 @@ function transferirDocumento(db)
 	return function (req, res) {
 		console.log("id del usuario que transferira el documento: " + req.decoded.id);
 		var documentos = db.get("documentos");
-		var IdUsuarioAntiguo = req.decoded.id;
+		IdDocumento = req.body.idDocumento;
 		var IdUsuarioNuevo = req.body.idUsuario;
 	
-		documentos.find({dueno:IdUsuarioAntiguo},function(err,docs){
-			if(err){
-				throw err;
-			}
-			else{
-				var IdDocumento = docs._id;
-				documentos.update({_id:IdDocumento},{$set:{dueno:IdUsuarioNuevo}},function (err) {
-				if (err){
-					throw err;
-				}
-				else{
-					res.send('Se transfirio el documento '+req.params.documentos+' con id '+IdDocumento + ' al usuario con id' + IdUsuarioNuevo);
-				}
-				});
-		    }
-	    });
+		documentos.update({_id:IdDocumento},{$set:{dueno:IdUsuarioNuevo}},function (err) {
+		if (err){
+			throw err;
+		}
+		else{
+			res.send('Se transfirio el documento '+req.params.documentos+' con id '+IdDocumento + ' al usuario con id' + IdUsuarioNuevo);
+		}
+		});
+	
 	}
 }
 
