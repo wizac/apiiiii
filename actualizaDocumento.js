a041017444981d4b25fc66dd3aea5abd3b6e6d4f
ggpq22 <ggpq13@hotmail.com>
2016-06-02 17:37:02 -0300

    gonza

--- actualizaDocumento.js
+++ actualizaDocumento.js
@@ -3,13 +3,16 @@ function actualizaDocumento(db)
 	return function (req, res) {
 		console.log("id del documento que se actualiza: " + req.body._id);
 		var documentos = db.get("documentos");
-		var IdDocumento = req.body._id;
-		documentos.update({_id:IdDocumento},{$set:req.body},function (err) {
+		var IdDocumento = req.body.idDocumento; //pasar por post el id del documento con el campo idDocumento
+		var actualizacion = req.body;
+		delete.actualizacion.idDocumento;
+		
+		documentos.update({_id:IdDocumento},{$set:actualizacion},function (err) {
 			if (err){
 				throw err;
 			}
 			else{ 
-			    res.send('Se actualizo el documento '+req.params.documentos+' con id '+IdDocumento);
+			    res.send('Se actualizo el documento con id '+IdDocumento);
 			}
 		});
 	}
--- transferirDocumento.js
+++ transferirDocumento.js
@@ -3,15 +3,15 @@ function transferirDocumento(db)
 	return function (req, res) {
 		console.log("id del usuario que transferira el documento: " + req.decoded.id);
 		var documentos = db.get("documentos");
-		IdDocumento = req.body.idDocumento;
-		var IdUsuarioNuevo = req.body.idUsuario;
-	
+		IdDocumento = req.body.idDocumento; // pasar por post el id del documento con el campo idDocumento
+		var IdUsuarioNuevo = req.body.idUsuario; // pasar por post el id del usuario al que se le dara el documento con el campo idUsuario
+		
 		documentos.update({_id:IdDocumento},{$set:{dueno:IdUsuarioNuevo}},function (err) {
 		if (err){
 			throw err;
 		}
 		else{
-			res.send('Se transfirio el documento '+req.params.documentos+' con id '+IdDocumento + ' al usuario con id' + IdUsuarioNuevo);
+			res.send('Se transfirio el documento '+' con id '+IdDocumento + ' al usuario con id' + IdUsuarioNuevo);
 		}
 		});
 	
