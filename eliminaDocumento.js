552b4cb84194aefa9983368d00653c14af9b0444
ggpq22 <ggpq13@hotmail.com>
2016-06-02 16:14:07 -0300

    gonza

--- eliminaDocumento.js
+++ eliminaDocumento.js
@@ -1,12 +1,18 @@
 function eliminaDocumento(db)
 {
-	console.log("id del documento que se elimina : " + req.body._id);
-	var documentos = db.get("documentos");
-	var IdDocumento = req.body._id;
-	documentos.remove({_id:IdDocumento}, function (err) {
-		if (err) throw err;
-		else res.send('Se elimino el documento '+req.params.documentos+' con id '+IdDocumento);
-	});
+	return function (req, res) {
+		console.log("id del documento que se elimina : " + req.body._id);
+		var documentos = db.get("documentos");
+		var IdDocumento = req.body._id;
+		documentos.remove({_id:IdDocumento}, function (err) {
+			if (err){
+				throw err;
+			}
+			else{
+				res.send('Se elimino el documento '+req.params.documentos+' con id '+IdDocumento);
+			}	
+		});
+	}
 }
 
-module.export = eliminaDocumento;
+exports.eliminaDocumento = eliminaDocumento;
--- transferirDocumento.js
+++ transferirDocumento.js
@@ -1,26 +1,28 @@
 function transferirDocumento(db)
 {
-	console.log("id del usuario que transferira el documento: " + req.decoded.id);
-	var documentos = db.get("documentos");
-	var IdUsuarioAntiguo = req.decoded.id;
-	var IdUsuarioNuevo = req.body.idUsuario;
+	return function (req, res) {
+		console.log("id del usuario que transferira el documento: " + req.decoded.id);
+		var documentos = db.get("documentos");
+		var IdUsuarioAntiguo = req.decoded.id;
+		var IdUsuarioNuevo = req.body.idUsuario;
 	
-	documentos.find({dueno:IdUsuarioAntiguo},function(err,docs){
-		if(err){
-			throw err;
-		}
-		else{
-			var IdDocumento = docs._id;
-			documentos.update({_id:IdDocumento},{$set:{dueno:IdUsuarioNuevo}},function (err) {
-			if (err){
+		documentos.find({dueno:IdUsuarioAntiguo},function(err,docs){
+			if(err){
 				throw err;
 			}
 			else{
-				res.send('Se transfirio el documento '+req.params.documentos+' con id '+IdDocumento + ' al usuario con id' + IdUsuarioNuevo);
-			}
-	        });
-		}
-	});
+				var IdDocumento = docs._id;
+				documentos.update({_id:IdDocumento},{$set:{dueno:IdUsuarioNuevo}},function (err) {
+				if (err){
+					throw err;
+				}
+				else{
+					res.send('Se transfirio el documento '+req.params.documentos+' con id '+IdDocumento + ' al usuario con id' + IdUsuarioNuevo);
+				}
+				});
+		    }
+	    });
+	}
 }
 
-module.export = transferirDocumento;
+export.transferirDocumento = transferirDocumento;
